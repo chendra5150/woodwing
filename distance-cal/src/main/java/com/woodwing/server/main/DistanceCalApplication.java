adc5bf4edd7d8167fa854895779c4cba7d380b72
chendra <chendra_harry@outlook.com>
2023-11-26 16:57:07 +0800

    - creating project base

---
+++ distance-cal/src/main/java/com/woodwing/server/main/DistanceCalApplication.java
@@ -0,0 +1,13 @@
+package com.woodwing.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceCalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistanceCalApplication.class, args);
+	}
+
+}
